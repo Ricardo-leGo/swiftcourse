2d59f40b117117f848872f8454286bf2c562a617
Ricardo Leyva Gonzalez <hola@legoricardo.com>
2023-02-13 23:24:09 -0600

    Initial Commit

---
+++ SwiftCourse/SwiftCourseApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftCourseApp.swift
+//  SwiftCourse
+//
+//  Created by Ricardo Leyva González on 13/02/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftCourseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
